314b3b15b79be7765e65e4ad8e6ce9575e76a764
Uherkovich Péter <uherkovich.peter@gmail.com>
2022-07-14 22:06:43 +0200

    phpdoc; vendor check

--- www/index.php
+++ www/index.php
@@ -1,8 +1,9 @@
-<?php
+<?php /** @noinspection PhpUnhandledExceptionInspection */
 /**
  * Application Web starter.
  * Copy this file to the application's web root as index.php
  */
+if(!file_exists(dirname(__DIR__) . '/vendor/autoload.php')) throw new Exception('Missing vendor library. Please run `composer install` first.');
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 $configFile = dirname(__DIR__) . '/config/config.php';
 // App class can be overridden here or in the main config
